86d9f8a020cadfc7bb50fa3f4b5852d4e6092052
shravanthy28 <parushashravanthy08@gmail.com>
2024-09-26 14:43:24 +0530

    Add files via upload

---
+++ age.py
@@ -0,0 +1,9 @@
+
+
+def greet_user():
+    name = input("What is your name? ")
+    age = input("How old are you? ")
+    print(f"Hello, {name}! You are {age} years old.")
+
+if __name__ == "__main__":
+    greet_user()
